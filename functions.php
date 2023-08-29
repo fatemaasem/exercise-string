ed2658bf046cb75a985019bb24fa40083525a77b
fatemaasem <fatemaasem232@gmail.com>
2023-08-29 22:55:40 +0200

    my change

--- functions.php
+++ functions.php
@@ -14,4 +14,26 @@ function make_string_first_character_uppercase($str){
 function make_string_first_character_of_all_the_words_uppercase($str){
     echo ucwords($str).".<br>";
 }
+function extract_the_file_name_from_the_string($path){
+   $file_name= strrchr($path,'/');
+   echo substr($file_name,1);
+}
+function extract_the_user_name_from_the_email_ID($email)
+{
+    $len=strpos($email,'@');
+    echo substr($email,0,$len);
+}
+function password_generate($chars) 
+{
+  $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+  return substr( str_shuffle($data),0,$chars);
+}
+function replace_the_first_given_str1_from_curren_str_with_given_str2($curren_str,$given_str1,$given_str2){
+   echo preg_replace('/'.$given_str1.'/',$given_str2,$curren_str,1);
+ 
+}
+function fun2(){
+    
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,41 +1,5 @@
 
-<html>
-  <head>
-    <title>Math Functions</title>
-</head>
-<body>
- <center>
-  <h3 style="color:gray;">find the maximum and minimum marks from the following set of arrays</h3>
-  </center>
-  <pre>
-Sample arrays :
-$marks1 = array(360,310,310,330,313,375,456,111,256);
-$marks2 = array(350,340,356,330,321);
-$marks3 = array(630,340,570,635,434,255,298);
-  </pre>
- <form method="get">
-  <lable for="num">Maximum marks </lable><br>
-  <input type="text" id="num" name="value1"><br>
-  <lable for="num">Minimum marks </lable><br>
-  <input type="text" id="num" name="value2"><br>
-  <input type="submit"name="form1Submit" value="check the answer" >
-  <?php 
-  include('functions.php');
- 
-if($_SERVER['REQUEST_METHOD'] === 'GET'&&$_GET['value1']==$array_contain_maximum_and_minimum[1]&&$_GET['value2']==$array_contain_maximum_and_minimum[0]){?>
-  <center><h3 style="color:green;">correct</h3>
-</center>
-<?php 
-}
-else {
-?>
-<center><h3 style="color:red;">wrong</h3>
-</center>
-<?php }?>
-  <form>
 
-</body>
-</html>
 <html>
   <body>
     <hr>
